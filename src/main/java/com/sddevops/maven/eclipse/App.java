b5e82ef0801e1c2294d0243653ca51a9fe72ec2d
jiunnhuat <soojh007@hotmail.com>
2026-06-23 20:52:59 +0800

    (feat) Add test line

--- src/main/java/com/sddevops/maven/eclipse/App.java
+++ src/main/java/com/sddevops/maven/eclipse/App.java
@@ -1,13 +1,12 @@
 package com.sddevops.maven.eclipse;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Soo Jiunn Huat!" );
+        System.out.println( "line 2" );
+        
     }
 }
