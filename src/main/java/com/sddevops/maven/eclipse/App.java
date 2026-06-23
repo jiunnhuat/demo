411fab7929bcdd05aedf0e38e11b6812921021f9
jiunnhuat <soojh007@hotmail.com>
2026-06-23 21:08:57 +0800

    Create new line

--- src/main/java/com/sddevops/maven/eclipse/App.java
+++ src/main/java/com/sddevops/maven/eclipse/App.java
@@ -6,7 +6,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Soo Jiunn Huat!" );
-        System.out.println( "line 2" );
+        System.out.println( "change line" );
         
     }
 }
